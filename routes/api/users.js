a8fd358e9b5ba4671b6f63799be27b8a99f55ee5
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-09-18 19:44:19 -0400

    passport integration requires fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -98,5 +98,16 @@ router.post('/login', (req, res) => {
   });
 });
 
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  '/current', passport.authenticate( 'jwt', { session: false }),
+  (req, res) => {
+      res.json({
+        msg: 'HHHeeeerrrrrreeeeessssss the carrant  USAR!!'
+      })
+});
+
 
 module.exports = router;
