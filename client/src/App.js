0e459e40718e6779ee986df5faae1611605001e1
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-09-23 19:56:50 -0400

    add sass and bootstrap, implement navbar template

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
